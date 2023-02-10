a53bf3ea5a0545a055895c997f52379a315be6c8
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 14:28:06 +0900

    Delete settings.php

--- settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configtext('nlrsbook/org_token', get_string('org_token', 'mod_nlrsbook'), "", null));
-}
